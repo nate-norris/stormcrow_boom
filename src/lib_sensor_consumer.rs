622184c3b8d60d91961912b433d6353d8bdb000d
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-02 10:43:46 -0800

    consumer library public exports added. need to figure out EdgeDetection

---
+++ src/lib_sensor_consumer.rs
@@ -0,0 +1,7 @@
+// pull EdgeDetection from external library
+//TODO see if needed, showing unused but was required previously
+pub use crate::lib_sensor::EdgeDetection;
+
+pub mod models;
+
+pub use models::EventRx;
