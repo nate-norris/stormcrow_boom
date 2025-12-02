7a9f5bd6fbbb857cb10175ac022f8687ce5abeb4
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-02 13:10:53 -0800

    testing with mock sensor

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ async fn main() -> anyhow::Result<()> {
     // tx and rx with buffer of 32 messages
     let (tx, rx): (EventTx, EventRx) = mpsc::channel(32);
 
-    let sensor = SoundSensor;
-    let _sensor = SoundSensorMock;
+    let _sensor = SoundSensor;
+    let sensor = SoundSensorMock;
     tokio::spawn(async move {
         if let Err(e) = sensor.detect_edge_task(tx.clone()).await {
             eprintln!("Sensor failed: {:?}", e);
