535f85f6b563944d61f163e25e97db9f2db3af12
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-02 12:57:55 -0800

    comment out unused import for now

--- src/lib_sensor_consumer.rs
+++ src/lib_sensor_consumer.rs
@@ -1,6 +1,6 @@
 // pull EdgeDetection from external library
 //TODO see if needed, showing unused but was required previously
-pub use crate::lib_sensor::EdgeDetection;
+// pub use crate::lib_sensor::EdgeDetection;
 
 pub mod models;
 pub mod consume;
