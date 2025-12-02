45209ebc4a203931e7827eb88ebcf94176ddd3cd
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-02 08:48:44 -0800

    making trait public for export to reveal internal fn

--- src/lib_sensor/_trait.rs
+++ src/lib_sensor/_trait.rs
@@ -1,10 +1,9 @@
 use std::pin::Pin;
 use std::future::Future;
-use anyhow::Result;
  
 use super::models::EventTx; // use crate::models::EventTx;
 
-trait SoundSensorT {
+pub trait SoundSensorT {
     fn detect_edge_task(&self, tx: EventTx) -> 
     Pin<Box<dyn Future<Output= anyhow::Result<()>> + Send>>;
 }
