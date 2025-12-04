91e44e595a81e43ec7dcb50759901ec342722a6b
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-04 09:20:03 -0800

    cleaned up, fixed proper spawn of mic_consume_task

--- src/main.rs
+++ src/main.rs
@@ -5,64 +5,59 @@ mod lib_sensor;
 mod lib_sensor_consumer;
 mod mm2t;
 mod mic;
-use lib_sensor::{EventTx, SoundSensor, SoundSensorMock, SoundSensorT};
+use lib_sensor::{EventTx, SoundSensorMock, SoundSensorT};
 use lib_sensor_consumer::{EventRx, sensor_consume_task};
 use mm2t::MM2THandle;
 use mic::{MicTx, MicRx, MicNotification, mic_consume_task};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // define channels
-    // sound sensor: tx and rx for EdgeDetection events
+    // sound sensor channels for EdgeDetection events
     let (tx, rx): (EventTx, EventRx) 
         = mpsc::channel(32);
-    // handle mic transmit and receive
-    //      notifications include sound sensor trigger, sound sensor error, 
-    //      and radio error
+    // mic channels for MicNotification events
     let (mic_tx, mic_rx): 
         (MicTx, MicRx) = mpsc::channel(32);
 
     // await for MicNotification
     // NOTE: mic is initialized before tasks so that it is ready to receive
-    mic_consume_task(mic_rx).await;
-    // swap which sensor will detect edge
-    let _sensor = SoundSensor; //hardware sound sensor
+    tokio::spawn(mic_consume_task(mic_rx));
+
+    // sensor for sound trigger
     let sensor = SoundSensorMock; //mock sensor for testing
 
     // initialize mm2t radio
     //      sends mic notification error if failed to start
-    let mic_tx = mic_tx.clone(); // shadow tx
     let radio = match MM2THandle::start().await {
-        Ok(r) => r, // assign to radio
+        Ok(r) => Arc::new(r), // assign to radio
         Err(_e) => {
             let _ = mic_tx.send(MicNotification::RadioError).await;
             return Ok(());
         }
     };
-    let radio = Arc::new(radio); //
 
     // spawn sound sensor thread for triggering edge detects
-    let mic_tx_spawn = mic_tx.clone();
+    let mic_tx_edge = mic_tx.clone();
     tokio::spawn(async move {
         // await sound sensor edge detect
         //      handle sound sensor error if occurs
         if let Err(_e) = sensor.detect_edge_task(tx.clone()).await {
-            let _ = mic_tx_spawn.send(MicNotification::SoundSensorError).await;
+            let _ = mic_tx_edge.send(MicNotification::SoundSensorError).await;
         }
     });
 
     // consume sound sensor receive
-    let mic_tx_spawn = mic_tx.clone(); // clone for mic tx
+    let mic_tx_consume = mic_tx.clone(); // clone for mic tx
     sensor_consume_task(rx, move || {
-        let mic_tx_spawn2 = mic_tx_spawn.clone();
-        let radio_spawn = Arc::clone(&radio);
+        let mic_tx = mic_tx_consume.clone();
+        let radio = Arc::clone(&radio);
         async move {
             //notify mic of shot success
-            let _ = mic_tx_spawn2.send(MicNotification::Boom).await;
+            let _ = mic_tx.send(MicNotification::Boom).await;
 
             // send radio packet and handle 
-            if let Err(_e) = radio_spawn.send_trigger_packet().await {
-                let _ = mic_tx_spawn2.send(MicNotification::RadioError).await;
+            if let Err(_e) = radio.send_trigger_packet().await {
+                let _ = mic_tx.send(MicNotification::RadioError).await;
             }
         }
     })
