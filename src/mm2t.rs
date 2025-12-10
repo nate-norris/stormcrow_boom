17dc9816da9a2634666a04dc6b4c8ed358ac89ba
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-10 10:11:25 -0800

    added checksum and actual byte packet

--- src/mm2t.rs
+++ src/mm2t.rs
@@ -4,11 +4,26 @@ use tokio_serial::{SerialStream, SerialPortBuilderExt, DataBits, Parity,
     StopBits, FlowControl}; //SerialStream
 use tokio::io::AsyncWriteExt;
 
-pub struct MM2THandle {
+pub struct MM2TBoomHandle {
     port: Mutex<SerialStream>
 }
-impl MM2THandle {
-    const PACKET: [u8; 3] = [0xAA, 0xBB, 0xCC];
+impl MM2TBoomHandle {
+
+    // packet contents
+    const SOP: u8 = 0xAA; // start of packet
+    const PACKET_TYPE: u8 = 0x42; //character 'B'... as in Boom
+    const PAYLOAD: [u8; 1] = [0x01];
+    const LENGTH_PAYLOAD: u8 = Self::PAYLOAD.len() as u8;
+    const CHECKSUM: u8 = Self::calc_checksum(Self::PACKET_TYPE, 
+        Self::LENGTH_PAYLOAD, &Self::PAYLOAD);//0x42;
+    // constructed packet
+    const PACKET: [u8; 5] = [
+        Self::SOP,
+        Self::PACKET_TYPE,
+        Self::LENGTH_PAYLOAD,
+        Self::PAYLOAD[0],
+        Self::CHECKSUM,
+    ];
 
     pub async fn start() -> anyhow::Result<Self> {
         //define parameters for opening serial port
@@ -33,5 +48,16 @@ impl MM2THandle {
         port.write_all(&Self::PACKET).await?;
         port.flush().await?;
         Ok(())
+    }
+
+    const fn calc_checksum(p_t: u8, l: u8, p: &[u8]) -> u8 {
+        let mut checksum = p_t ^ l;
+
+        let mut i = 0;
+        while i < p.len() {
+            checksum ^= p[i];
+            i += 1;
+        }
+        checksum
     }
 }
