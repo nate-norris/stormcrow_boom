0e966d7e43f194f2545fd5f5ef615b007801a3b5
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-11 05:51:37 -0800

    added documentation

--- src/lib_sensor/models.rs
+++ src/lib_sensor/models.rs
@@ -1,8 +1,11 @@
 use tokio::sync::mpsc;
 
+// Notification events emitted by sound sensor hardware
 pub enum EdgeDetection {
-    Triggered,
-    Error(String),
+    Triggered, // sound sensor has detected an event
+    Error(String), // error in physical sound sensor
 }
 
+// mpsc channels for EdgeDetection types
+// used by producers to send edge events
 pub type EventTx = mpsc::Sender<EdgeDetection>;
