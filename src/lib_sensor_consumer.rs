50f04b0303a743f7e0c33cfe08bda51d18efbe61
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-11 10:23:09 -0800

    add documentation comments

--- src/lib_sensor_consumer.rs
+++ src/lib_sensor_consumer.rs
@@ -1,6 +1,33 @@
-// pull EdgeDetection from external library
-//TODO see if needed, showing unused but was required previously
-// pub use crate::lib_sensor::EdgeDetection;
+//! # lib_sensor_consumer
+//!
+//! `lib_sensor_consumer` handles events triggered through mpsc channel EventTx
+//! that are passed to EventRx. Upon receiving an EventRx the provided function
+//! is executed. EdgeDetection events are pulled from `lib_sensor`.
+//!
+//! - **Event channel** `EventRx``
+//! - **Sound sensor consumer task** (`sensor_consume_task`) for handling events
+//! 
+//! ## Usage
+//!
+//! ```no_run
+//! use lib_sensor::{EdgeDetection, EventTx, SoundSensorMock, SoundSensorT};
+//! use lib_sensor_consumer::{sensor_consume_task, EventRx};
+//! use tokio::sync::mpsc;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let (tx, rx) = mpsc::channel(32);
+//!     let sensor = SoundSensorMock;
+//!
+//!     // Spawn the sensor simulation
+//!     tokio::spawn(sensor.detect_edge_task(tx));
+//!
+//!     // Spawn the consumer task
+//!     tokio::spawn(sensor_consume_task(rx, || async {
+//!         println!("Trigger action executed!");
+//!     }));
+//! }
+//! ```
 
 pub mod models;
 pub mod consume;
