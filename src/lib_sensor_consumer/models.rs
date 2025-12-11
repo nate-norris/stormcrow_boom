b11c43cbfc33a92befac463818a38fabab21363b
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2025-12-11 08:32:01 -0800

    added documentation comments

--- src/lib_sensor_consumer/models.rs
+++ src/lib_sensor_consumer/models.rs
@@ -2,4 +2,6 @@ use tokio::sync::mpsc;
 
 use crate::lib_sensor::EdgeDetection;
 
+// mpsc channel for EdgeDetection type
+// used by consumer to process edge events
 pub type EventRx = mpsc::Receiver<EdgeDetection>;
