33864df2129c4c5eb8bf03d4a1f3945e478813d3
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-01-20 11:33:25 -0800

    removed tracing subscriber logging

--- src/logger.rs
+++ src/logger.rs
@@ -1,14 +1,7 @@
 //! Starts async file-based log service for a headless program.
 //! 
-//! The global 'tracing' subscriber writes log messages to a "log.txt" file 
-//! co-located with the running executable. A background WorkerGuard handles
-//! I/O so logging does not block the main program. The file is created automatically 
-//! if missing.  
-//! 
-//! 
-//! 
-//! 
-
+//! The global simplelog WriteLogger writes log messages to a "stormcrow.log" 
+//! file co-located with the running executable or at a user specified location.
 
 use std::env;
 use std::path::{PathBuf, Path};
@@ -16,7 +9,16 @@ use std::fs::OpenOptions;
 use simplelog::*;
 use log::LevelFilter;
 
-
+/// Starts async file-based log service for a headless program.
+/// The file is created automatically if missing. 
+/// 
+/// # Usage
+/// This should be called once at program startup, typically from `main()`:
+///
+/// ```no_run
+/// logger::init_logger();
+/// logger::info("Application started");
+/// ```
 pub fn init_logger(dir_path: Option<&Path>) {
     
     // determine director for log file
@@ -73,101 +75,3 @@ pub fn error(message: impl std::fmt::Display, extra: Option<impl std::fmt::Displ
         None => log::error!("{}", message),
     }
 }
-
-
-// use std::sync::Once;
-// use std::fs;
-
-// use tracing::Level;
-// use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
-// use tracing_subscriber::fmt::time::UtcTime;
-
-// // ensure the logger is initalized once
-// static INIT: Once = Once::new();
-
-// // Non blocking logging gaurd. Initialized once and never mutated.
-// //
-// // Keeping this alive ensures logging threads remain active for the life of
-// //      the program.
-// static mut LOG_GUARD: Option<WorkerGuard> = None;
-
-// /// Starts async file-based log service for a headless program.
-// ///
-// /// The global 'tracing' subscriber writes log messages to a "log.txt" file 
-// /// co-located with the running executable. A background WorkerGuard handles
-// /// I/O so logging does not block the main program. The file is created automatically 
-// /// if missing. 
-// /// 
-// /// # Usage
-// /// This should be called once at program startup, typically from `main()`:
-// ///
-// /// ```no_run
-// /// logger::init_logger();
-// /// logger::info("Application started");
-// /// ```
-// pub fn init_logger_old() {
-//     INIT.call_once( || {
-
-//         // path to current executable file
-//         let exe_dir = env::current_exe()
-//             .ok()
-//             .and_then(|p| p.parent().map(PathBuf::from))
-//             .unwrap_or_else(|| env::current_dir().unwrap());
-
-//         let log_path = exe_dir.join("log.txt");
-//         //create the file if needed
-//         if !log_path.exists() {
-//             fs::File::create(&log_path).unwrap();
-//         }
-
-//         // open file
-//         let file = OpenOptions::new()
-//             .create(true)
-//             .append(true)
-//             .open(log_path)
-//             .unwrap();
-
-//         // create non-blocking writer and guard
-//         let (nb, g) = NonBlocking::new(file);
-//         unsafe { LOG_GUARD = Some(g); }
-
-//         // global tracing subscriber
-//         tracing_subscriber::fmt()
-//             .with_writer(nb)
-//             .compact() // single line
-//             .with_timer(UtcTime::rfc_3339())
-//             .with_max_level(Level::INFO)
-//             .with_level(true)
-//             .with_target(false)
-//             .with_ansi(false) // no color code
-//             .init();
-//     });
-// }
-
-// /// Writes information logs
-// /// Arguments must implement ['std::fmt:;Display']
-// /// 
-// /// # Arguments
-// /// * `message` — The information to log
-// pub fn infoold(message: impl std::fmt::Display) {
-//     tracing::info!("{}", message);
-// }
-
-// /// Writes error logs
-// /// Arguments must implement ['std::fmt:;Display']
-// /// 
-// /// # Arguments
-// /// `message` - the primary error text
-// /// `extra` - an additional value to append to log
-// /// 
-// /// # Examples
-// /// ```
-// /// logger::error("sensor failure", None);
-// /// logger::error("sensor failure:", Some(err));
-// /// ```
-// pub fn errorold(message: impl std::fmt::Display, extra: Option<impl std::fmt::Display>) {
-//     match extra {
-//         Some(e) => tracing::error!("{} {}", message, e),
-//         None => tracing::error!("{}", message),
-//     }
-// }
