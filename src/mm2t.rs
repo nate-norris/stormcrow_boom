8975e22514c328b5a19d840c113b657801583f33
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-01-21 10:14:14 -0800

    removed packet logic from the mm2t

--- src/mm2t.rs
+++ src/mm2t.rs
@@ -4,53 +4,33 @@ use tokio_serial::{SerialStream, SerialPortBuilderExt, DataBits, Parity,
     StopBits, FlowControl}; //SerialStream
 use tokio::io::AsyncWriteExt;
 
-/// Represents a handle to an MM2T device capable of sending "Boom" trigger packets
-/// over a serial connection.
+/// Represents a handle to an MM2T device over a serial connection.
 ///
 /// This struct manages a serial port connection internally via a `Mutex` for
 /// safe asynchronous access. It provides methods to initialize the device and
-/// send a predefined packet.
+/// send a bytes.
 ///
 /// # Example
 /// ```rust,no_run
-/// use mm2t::MM2TBoomHandle;
+/// use mm2t::MM2TTransport;
 /// use tokio;
 ///
 /// #[tokio::main]
 /// async fn main() -> anyhow::Result<()> {
 ///     // Initialize the MM2T device
-///     let boom_handle = MM2TBoomHandle::start().await?;
+///     let mm2t = MM2TTransport::start("/dev/ttyUSB0").await?;
 ///
 ///     // Send a trigger packet
-///     boom_handle.send_trigger_packet().await?;
+///     mm2t.send(&[0x01]).await?;
 ///
 ///     Ok(())
 /// }
 /// ```
-pub struct MM2TBoomHandle {
+pub struct MM2TTransport {
     port: Mutex<SerialStream>
 }
 
-impl MM2TBoomHandle {
-
-    // packet contents
-    const SOP: u8 = 0xAA; // start of packet
-    const PACKET_TYPE: u8 = 0x42; //character 'B'... as in Boom
-    const PAYLOAD: [u8; 1] = [0x01];
-    const LENGTH_PAYLOAD: u8 = Self::PAYLOAD.len() as u8;
-    // precalculated checksum
-    // NOTE: including logic for calculation for reverse engineering
-    //      on receiving side.
-    const CHECKSUM: u8 = Self::calc_checksum(Self::PACKET_TYPE, 
-        Self::LENGTH_PAYLOAD, &Self::PAYLOAD);//0x42;
-    // constructed packet
-    const PACKET: [u8; 5] = [
-        Self::SOP,
-        Self::PACKET_TYPE,
-        Self::LENGTH_PAYLOAD,
-        Self::PAYLOAD[0],
-        Self::CHECKSUM,
-    ];
+impl MM2TTransport {
 
     /// opens a serial connection to the MM2T device
     pub async fn start(port_name: &str) -> anyhow::Result<Self> {
@@ -70,25 +50,12 @@ impl MM2TBoomHandle {
         })
     }
 
-    /// sends the predefined "Boom" packet
-    pub async fn send_trigger_packet(&self) -> anyhow::Result<()> {
-        
+    /// send bytes over the port
+    pub async fn send(&self, bytes: &[u8]) -> anyhow::Result<()> {
         let mut port = self.port.lock().await;
-        port.write_all(&Self::PACKET).await?;
+        port.write_all(bytes).await?;
         port.flush().await?;
-        println!("mm2t: sent packet {:?}", &Self::PACKET);
+
         Ok(())
-    }
-
-    /// internal helper for for checksum calculation
-    const fn calc_checksum(p_t: u8, l: u8, p: &[u8]) -> u8 {
-        let mut checksum = p_t ^ l;
-
-        let mut i = 0;
-        while i < p.len() {
-            checksum ^= p[i];
-            i += 1;
-        }
-        checksum
     }
 }
