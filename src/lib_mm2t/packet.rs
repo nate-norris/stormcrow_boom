c8cde83d2338f8c1e0f8a018b49b306f77f440d1
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-01-22 06:16:03 -0800

    SOP moved to packet_decoder

--- src/lib_mm2t/packet.rs
+++ src/lib_mm2t/packet.rs
@@ -1,7 +1,7 @@
 use super::checksum::checksum;
+use super::packet_decoder::SOP;
 
 pub trait PacketT {
-    const SOP: u8 = 0xAA; // start of packet
 
     fn packet_type(&self) -> u8;
 
@@ -12,7 +12,7 @@ pub trait PacketT {
         //      length of payload, and checksum
         let mut buf = Vec::with_capacity(self.payload().len() + 4);
         // push bytes onto buffer
-        buf.push(Self::SOP); //start of packet
+        buf.push(SOP); //start of packet
         buf.push(self.packet_type()); //packet type
         buf.push(self.payload().len() as u8); // payload length
         buf.extend_from_slice(self.payload());
